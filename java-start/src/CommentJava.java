0d3507f90ace0435486c566992214331d90a97aa
danii0110 <105475360+danii0110@users.noreply.github.com>
2023-12-23 23:21:08 +0900

    주석(comment)

---
+++ java-start/src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+    public static void main(String[] args) { //단축방법 -> psvm
+        System.out.println(); //단축방법 -> sout
+        System.out.println("hello java1"); //hello java1을 출력합니다.
+        //System.out.println("hello java2");
+
+        /*
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+         */
+    }
+}
