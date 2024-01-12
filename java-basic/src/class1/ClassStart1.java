83b7bea4291d01f7af24148d393131bfbff2eae9
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-01-12 13:38:00 +0900

    first commit

---
+++ java-basic/src/class1/ClassStart1.java
@@ -0,0 +1,20 @@
+package class1;
+
+public class ClassStart1 {
+    public static void main(String[] args) {
+        String student1Name = "학생1";
+        int student1Age = 15;
+        int student1Grade = 90;
+
+        String student2Name = "학생2";
+        int student2Age = 16;
+        int student2Grade = 80;
+    }
+//    public static void main(String[] args) {
+//        showInfo("학생1", 15, 90);
+//        showInfo("학생2", 16, 80);
+//    }
+//    public static void showInfo (String name, int age, int score) {
+//        System.out.println("이름: " + name + " 나이: " + age + " 성적: " + score);
+//    }
+}
