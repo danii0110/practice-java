8b957666060ec89bf425d52c332ed7d394e021b9
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-01-13 21:43:11 +0900

    section2 기본형과 참조형

---
+++ java-basic/src/ref/BigData.java
@@ -0,0 +1,6 @@
+package ref;
+
+public class BigData {
+    Data data; //참조형은 초깃값 null
+    int count; //int는 초깃값 0
+}
---
+++ java-basic/src/ref/NullMain4.java
@@ -0,0 +1,18 @@
+package ref;
+
+public class NullMain4 {
+    public static void main(String[] args) {
+        BigData bigData = new BigData();
+        bigData.data = new Data();
+        System.out.println("bigData.count = " + bigData.count);
+        System.out.println("bigData.data = " + bigData.data);
+
+        //NullPointerException
+        System.out.println("bigData.data.value = " + bigData.data.value);
+        //예외 발생 과정
+        //bigData.data.value
+        //x001.data.value //bigData는 x001 참조값을 가진다.
+        //null.value //x001.data는 null값을 가진다.
+        //NullPointerException //null값에 .(dot)을 찍으면 예외가 발생한다.
+    }
+}
