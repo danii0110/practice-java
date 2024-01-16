aa2f81281081288bef3db959811b706eda54ca10
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-01-16 13:46:23 +0900

    section4 생성자 완료:
    q
    
    exit

---
+++ java-basic/src/construct/MemberConstruct.java
@@ -0,0 +1,23 @@
+package construct;
+
+public class MemberConstruct {
+    String name;
+    int age;
+    int grade;
+
+    //추가
+    MemberConstruct(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//        this.grade = 50;
+        this(name, age, 50); //변경
+        //this()는 생성자 코드의 첫줄에만 작성할 수 있다.
+    }
+
+    MemberConstruct(String name, int age, int grade) {
+        System.out.println("생성자 호출 name=" + name + ",age=" + age + ",grade=" + grade);
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+}
