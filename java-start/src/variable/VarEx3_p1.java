0ca3565b9480735816e2f54dcb45dd98075a2f76
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-04-02 09:03:53 +0900

    section2 변수 복습

---
+++ java-start/src/variable/VarEx3_p1.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class VarEx3_p1 {
+    public static void main(String[] args) {
+        long longNum = 10000000000L;
+        System.out.println("longNum = " + longNum);
+        
+        boolean isTrue = true;
+        System.out.println("isTrue = " + isTrue);
+    }
+}
