5ab768be6e372d62a3d713732ceb812aa12ac89a
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-04-03 10:05:20 +0900

    section3 연산자 복습

---
+++ java-start/src/operator/OperationEx2_p1.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class OperationEx2_p1 {
+    public static void main(String[] args) {
+        double val1 = 1.5;
+        double val2 = 2.5;
+        double val3 = 3.5;
+
+        double sum = val1 + val2 + val3;
+        double ave = sum / 3;
+
+        System.out.println("sum = " + sum);
+        System.out.println("ave = " + ave);
+    }
+}
